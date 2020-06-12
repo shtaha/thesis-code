update: test + units + bug in data (development mode)

lib/dc_opf.py
@@ -1,7 +1,53 @@
 import numpy as np
 import pandapower as pp
+import pandas as pd
 import pyomo.environ as pyo
 import pyomo.opt as pyo_opt
+
+
+class PyomoMixin:
+    pass
+
+
+class UnitConverter:
+
+    def __init__(self, base_unit_p=1e6, base_unit_v=1e5):
+        # Base units
+        self.base_unit_p = base_unit_p  # Base unit is 1 MVA or 1 MW or 1 MVar
+        self.base_unit_v = base_unit_v  # Base unit is 100 kV
+        self.base_unit_a = self.base_unit_p / self.base_unit_v  # Base unit is 1 A
+        self.base_unit_z = self.base_unit_v ** 2 / self.base_unit_p
+
+        self.unit_p_mw = 1e6
+        self.unit_a_ka = 1e3
+        self.unit_v_kv = 1e3
+
+    def print_converted(self):
+        print("base units")
+        print("{:<10}{}".format("unit p", self.base_unit_p))
+        print("{:<10}{}".format("unit v", self.base_unit_v))
+        print("{:<10}{}".format("unit a", self.base_unit_a))
+        print("{:<10}{}".format("unit z", self.base_unit_z))
+
+    def convert_mw_to_per_unit(self, p_mw):
+        p_pu = p_mw * self.unit_p_mw / self.base_unit_p
+        return p_pu
+
+    def convert_ohm_to_per_unit(self, z_ohm):
+        z_pu = z_ohm / self.base_unit_z
+        return z_pu
+
+    def convert_a_to_per_unit(self, i_a):
+        i_pu = i_a / self.base_unit_a
+        return i_pu
+
+    def convert_ka_to_per_unit(self, i_ka):
+        i_pu = i_ka * self.unit_a_ka / self.base_unit_a
+        return i_pu
+
+    def convert_kv_to_per_unit(self, v_kv):
+        v_pu = v_kv * self.unit_v_kv / self.base_unit_v
+        return v_pu
 
 
 class DCOptimalPowerFlow(object):
@@ -9,23 +55,19 @@ class DCOptimalPowerFlow(object):
     Constructor for DC-OPF given the environment and the observation.
     """
 
-    def __init__(self, env, solver="gurobi"):
+    def __init__(self, env, solver="gurobi", verbose=False):
         """
         Initialize DC-OPF Parameters given an environment: information on topology, generators, loads, and lines.
         """
         self.env = env
         self.backend = env.backend
         self.grid = env.backend._grid
+        self.unit_converter = UnitConverter(base_unit_v=self.grid.bus["vn_kv"].values[0] * 1000)  # TODO: Hack
 
         self.bus = self.grid.bus
         self.gen = self.grid.gen
         self.load = self.grid.load
         self.line = self.grid.line
-
-        # Base units
-        self.unit_p = 1000000  # Base unit is 1 MVA or 1 MW or 1 MVar
-        self.unit_a = 1.0  # Base unit is 1 A
-        self.unit_v = 1000  # Base unit is 1 kV
 
         # Grid parameters
         self.n_bus = None
@@ -38,33 +80,36 @@ class DCOptimalPowerFlow(object):
         # Generator parameters
         self.n_gen = None
         self.gen_ids = None
-        self.gen_p_max = None
-        self.gen_p_min = None
+        self.gen_p_max = None  # In p.u.
+        self.gen_p_min = None  # In p.u.
         self.gen_ids_to_bus_ids = None  # VARIABLE
         self.bus_ids_to_gen_ids = None  # VARIABLE
+        self.gen_costs = None  # TEST VARIABLE
 
         # Load parameters
         self.n_load = None
         self.load_ids = None
         self.load_ids_to_bus_ids = None  # VARIABLE
-        self.load_p = None  # VARIABLE
+        self.load_p = None  # In p.u. # VARIABLE
         self.bus_ids_to_load_ids = None  # VARIABLE
 
         # Line parameters
         self.n_line = None
         self.line_ids = None
-        self.line_resistance = None
-        self.line_reactance = None
-        self.line_inverse_reactance = None
-        self.line_i_max = None
-        self.line_p_max = None
+        self.line_resistance = None  # In p.u.
+        self.line_reactance = None  # In p.u.
+        self.line_inverse_reactance = None  # In p.u.
+        self.line_i_max = None  # In p.u.
+        self.line_p_max = None  # In p.u.
         self.line_ids_to_bus_ids = None  # VARIABLE
+        self.line_ids_to_or_bus_ids = None  # VARIABLE
+        self.line_ids_to_ex_bus_ids = None  # VARIABLE
 
         # Initialize parameter values
-        self.initialize_grid()
-        self.initialize_generators()
-        self.initialize_loads()
-        self.initialize_lines()
+        self.initialize_grid(verbose=verbose)
+        self.initialize_generators(verbose=verbose)
+        self.initialize_loads(verbose=verbose)
+        self.initialize_lines(verbose=verbose)
 
         # DC-OPF model
         self.model = None
@@ -73,11 +118,11 @@ class DCOptimalPowerFlow(object):
         self.initialize_model()
         self.solver = pyo_opt.SolverFactory(solver)
 
-        self.print_model()
+        self.print_model(verbose=verbose)
 
-    def print_model(self):
-        # print(self.model.display())
-        print(self.model.pprint())
+    def print_model(self, verbose=False):
+        if verbose:
+            print(self.model.pprint())
 
     def initialize_model(self):
         self.model = pyo.ConcreteModel("DC-OPF Model")
@@ -92,6 +137,11 @@ class DCOptimalPowerFlow(object):
                                          within=pyo.NonNegativeReals)
         self.model.gen_p_min = pyo.Param(self.model.gen_set,
                                          initialize=self._create_map_ids_to_values(self.gen_ids, self.gen_p_min),
+                                         within=pyo.NonNegativeReals)
+
+        self.model.gen_costs = pyo.Param(self.model.gen_set,
+                                         initialize=self._create_map_ids_to_values(
+                                             self.gen_ids, self.gen_costs),
                                          within=pyo.NonNegativeReals)
 
         self.model.line_p_max = pyo.Param(self.model.line_set,
@@ -129,10 +179,10 @@ class DCOptimalPowerFlow(object):
         self.model.constraint_gen_p = pyo.Constraint(self.model.gen_set, rule=self._constraint_gen_p)
 
         # Bound line power flow
-        self.model.constraint_line_p = pyo.Constraint(self.model.gen_set, rule=self._constraint_line_p)
+        self.model.constraint_line_p = pyo.Constraint(self.model.line_set, rule=self._constraint_line_p)
 
         # Define line power flow
-        self.model.constraint_line_p_def = pyo.Constraint(self.model.gen_set, rule=self._constraint_line_p_def)
+        self.model.constraint_line_p_def = pyo.Constraint(self.model.line_set, rule=self._constraint_line_p_def)
 
         # Set delta[0] = 0
         self.model.constraint_delta = pyo.Constraint(self.model.bus_set, rule=self._constraint_delta)
@@ -143,6 +193,9 @@ class DCOptimalPowerFlow(object):
         # Bus power balance constraints
         self.model.constraint_bus_balance_p = pyo.Constraint(self.model.bus_set, rule=self._constraint_bus_balance_p)
 
+        # Objectives
+        self.model.objective = pyo.Objective(rule=self._objective_gen_p, sense=pyo.minimize)
+
     def update(self, obs):
         """
         Update DC-OPF parameters given a new observation.
@@ -151,10 +204,34 @@ class DCOptimalPowerFlow(object):
         """
         pass
 
-    def dc_opf_backend(self, **kwargs):
-        pp.rundcopp(self.grid, **kwargs)
+    def set_gen_cost(self, gen_costs):
+        gen_costs = np.array(gen_costs).flatten()
+        assert gen_costs.size == self.gen_costs.size  # Check dimensions
 
-    def initialize_grid(self):
+        self.gen_costs = gen_costs
+
+    def solve_dc_opf_backend(self, verbose=False, **kwargs):
+        for gen_id in self.gen_ids:
+            pp.create_poly_cost(self.grid, gen_id, "gen", cp1_eur_per_mw=self.gen_costs[gen_id])
+
+        pp.rundcopp(self.grid, verbose=verbose, **kwargs)
+
+        if not verbose:
+            print("\nDC-OPF solved by backend")
+            print("{:<10}{}".format("cost", self.grid.res_cost))
+            self._print_res_gen(self.grid.res_gen["p_mw"], self.gen_costs)
+
+    def solve_dc_opf(self, verbose=False):
+        _ = self.solver.solve(self.model, tee=verbose)
+
+        if verbose:
+            print(self.model.display())
+        else:
+            print("\nDC-OPF solved")
+            print("{:<10}{}".format("cost", pyo.value(self.model.objective)))
+            self._print_res_gen(self._access_pyomo_variable(self.model.gen_p), self.gen_costs)
+
+    def initialize_grid(self, verbose=False):
         self.n_bus = self.bus.shape[0]
         self.bus_ids = self.bus.index.values  # [0, 1, ..., n_bus-1] (n_bus, )
 
@@ -164,49 +241,116 @@ class DCOptimalPowerFlow(object):
         self.bus_ids_to_sub_ids = np.tile(self.sub_ids, 2)  # [0, 1, ..., n_sub-1, 0, 1, ..., n_sub-1] (n_bus, )
         self.bus_ids_to_sub_bus_ids = np.tile([1, 2], self.n_sub)  # [1, 2, 1, 2, ..., 1, 2] (n_sub, )
 
-    def initialize_generators(self):
+        if verbose:
+            print("initializing grid ...")
+            print("{:<30}{}\t{}".format("sub", self.sub_ids, self.n_sub))
+            print("{:<30}{}\t{}".format("bus", self.bus_ids, self.n_bus))
+            print("{:<30}{}".format("bus_ids_to_sub_ids", self.bus_ids_to_sub_ids))
+            print("{:<30}{}\n".format("bus_ids_to_sub_bus_ids", self.bus_ids_to_sub_bus_ids))
+
+        assert self.n_bus == 2 * self.n_sub  # Each substation has 2 buses
+
+    def initialize_generators(self, verbose=False):
         self.n_gen = self.env.n_gen
         self.gen_ids = self.gen.index.values
 
-        self.gen_p_max = self.env.gen_pmax
-        self.gen_p_min = self.env.gen_pmin
+        self.gen_p_max = self.unit_converter.convert_mw_to_per_unit(self.env.gen_pmax)  # In p.u.
+        self.gen_p_min = self.unit_converter.convert_mw_to_per_unit(self.env.gen_pmin)  # In p.u.
 
         self.gen_ids_to_bus_ids = self.gen["bus"].values
 
         self.bus_ids_to_gen_ids = [list(np.equal(self.gen_ids_to_bus_ids, bus_id).nonzero()[0]) for bus_id in
                                    self.bus_ids]
 
-    def initialize_loads(self):
+        if verbose:
+            print("initializing generators ...")
+            print("{:<30}{}\t{}".format("gen", self.gen_ids, self.n_gen))
+            print("{:<30}{} p.u.".format("gen_p_max", self.gen_p_max))
+            print("{:<30}{} p.u.".format("gen_p_min", self.gen_p_min))
+            print("{:<30}{}".format("gen_ids_to_bus_ids", self.gen_ids_to_bus_ids))
+            print("{:<30}{}\n".format("bus_ids_to_gen_ids", self.bus_ids_to_gen_ids))
+
+        # Initial value
+        self.gen_costs = np.ones_like(self.gen_ids)
+
+        assert self.n_gen == self.gen.shape[0]  # Check number of generators
+        # assert np.equal(self.unit_converter.convert_mw_to_per_unit(self.gen["max_p_mw"]),
+        #                 self.gen_p_max).all()  # Check if grid.json and prods_char.csv match
+        # assert np.equal(self.unit_converter.convert_mw_to_per_unit(self.gen["min_p_mw"]),
+        #                 self.gen_p_min).all()  # Check if grid.json and prods_char.csv match
+
+    def initialize_loads(self, verbose=False):
         self.n_load = self.env.n_load
         self.load_ids = self.load.index.values
 
-        self.load_p = self.load["p_mw"].values
+        self.load_p = self.unit_converter.convert_mw_to_per_unit(self.load["p_mw"].values)  # In p.u.
 
         self.load_ids_to_bus_ids = self.load["bus"].values
 
         self.bus_ids_to_load_ids = [list(np.equal(self.load_ids_to_bus_ids, bus_id).nonzero()[0]) for bus_id in
                                     self.bus_ids]
 
-    def initialize_lines(self):
+        if verbose:
+            print("initializing loads ...")
+            print("{:<30}{}\t{}".format("load", self.load_ids, self.n_load))
+            print("{:<30}{}".format("load_p", self.load_p))
+            print("{:<30}{}".format("load_ids_to_bus_ids", self.load_ids_to_bus_ids))
+            print("{:<30}{}\n".format("bus_ids_to_load_ids", self.bus_ids_to_load_ids))
+
+        assert self.n_load == self.load.shape[0]  # Check number of loads
+        # assert np.equal(self.unit_converter.convert_mw_to_per_unit(self.env.get_obs().load_p),
+        #                 self.load_p).all()  # Check consistency between new obs and env
+
+    def initialize_lines(self, verbose=False):
         self.n_line = self.env.n_line
         self.line_ids = self.line.index.values
 
-        self.line_resistance = np.divide(np.multiply(self.line["r_ohm_per_km"].values, self.line["length_km"]),
-                                         self.line["parallel"])
-
-        self.line_reactance = np.divide(np.multiply(self.line["x_ohm_per_km"].values, self.line["length_km"]),
-                                        self.line["parallel"])  # Reactance in Ohms
-        self.line_inverse_reactance = np.divide(1, self.line_reactance)  # Negative susceptance in Siemens/Mhos
-
-        self.line_i_max = self.env.get_thermal_limit() / self.unit_a  # Line current limit in Amperes
-        self.line_p_max = np.multiply(np.square(self.line_i_max * self.unit_a),
-                                      self.line_resistance) / self.unit_p  # Line thermal limit in MW
-
         # [(bus_or, bus_ex), ..., (bus_or, bus_ex)] (n_line, )
         self.line_ids_to_bus_ids = list(zip(self.line["from_bus"], self.line["to_bus"]))
+        self.line_ids_to_or_bus_ids = self.line["from_bus"].values
+        self.line_ids_to_ex_bus_ids = self.line["to_bus"].values
+
+        line_length = self.line["length_km"].values
+        line_parallel = self.line["parallel"].values
+
+        if "max_loading_percent" in self.line.columns:
+            line_max_loading = self.line["max_loading_percent"].values / 100.0
+        else:
+            line_max_loading = np.ones((self.n_line,))
+
+        self.line_resistance = np.divide(np.multiply(self.line["r_ohm_per_km"].values, line_length),
+                                         line_parallel)  # In Ohms
+        self.line_resistance = self.unit_converter.convert_ohm_to_per_unit(self.line_resistance)
+
+        self.line_reactance = np.divide(np.multiply(self.line["x_ohm_per_km"].values, line_length),
+                                        line_parallel)  # Reactance in Ohms
+        self.line_reactance = self.unit_converter.convert_ohm_to_per_unit(self.line_reactance)  # In p.u.
+
+        self.line_inverse_reactance = np.divide(1, self.line_reactance)  # Negative susceptance in p.u.
+
+        self.line_i_max = np.multiply(self.env.get_thermal_limit(), line_max_loading)  # Line current limit in Amperes
+        self.line_i_max = self.unit_converter.convert_a_to_per_unit(self.line_i_max)  # In p.u.
+
+        # TODO: HACK ONLY LINE OR!
+        self.line_p_max = self.line_i_max * self.unit_converter.convert_kv_to_per_unit(
+            self.bus["vn_kv"].values[self.line_ids_to_or_bus_ids])  # In p.u.
+
+        if verbose:
+            print("initializing lines ...")
+            print("{:<30}{}\t{}".format("line", self.line_ids, self.n_line))
+            print("{:<30}{}".format("line_resistance", self.line_resistance))
+            print("{:<30}{}".format("line_reactance", self.line_reactance))
+            print("{:<30}{}".format("line_inverse_reactance", self.line_inverse_reactance))
+            print("{:<30}{}".format("line_i_max", self.line_i_max))
+            print("{:<30}{}".format("line_p_max", self.line_p_max))
+            print("{:<30}{}\n".format("line_ids_to_bus_ids", self.line_ids_to_bus_ids))
+
+        assert self.n_line == self.line.shape[0]  # Check number of lines
+        # assert np.equal(self.unit_converter.convert_ka_to_per_unit(self.line["max_i_ka"]),
+        #                 self.line_i_max).all()  # Check consistency of thermal limits
 
     """
-        Helpers.
+        Pyomo modeling functions.
     """
 
     @staticmethod
@@ -240,13 +384,24 @@ class DCOptimalPowerFlow(object):
         if len(bus_gen_p):
             return model.bus_gen_p[bus_id] == sum(bus_gen_p)
         else:
-            return pyo.Constraint.Skip
+            # If no generator bus injections
+            return model.bus_gen_p[bus_id] == 0
 
     @staticmethod
     def _constraint_bus_balance_p(model, bus_id):
         return model.bus_gen_p[bus_id] - model.bus_load_p[bus_id] == \
-            sum([model.line_p[line_id] for line_id in model.line_set if bus_id == model.line_ids_to_bus_ids[line_id][0]]) + \
-            sum([model.line_p[line_id] for line_id in model.line_set if bus_id == model.line_ids_to_bus_ids[line_id][1]])
+               sum([model.line_p[line_id] for line_id in model.line_set if
+                    bus_id == model.line_ids_to_bus_ids[line_id][0]]) - \
+               sum([model.line_p[line_id] for line_id in model.line_set if
+                    bus_id == model.line_ids_to_bus_ids[line_id][1]])
+
+    @staticmethod
+    def _objective_gen_p(model):
+        return sum([model.gen_p[gen_id] * model.gen_costs[gen_id] for gen_id in model.gen_set])
+
+    """
+        Pyomo helper functions.
+    """
 
     @staticmethod
     def _create_map_ids_to_values_sum(ids, sum_ids, values):
@@ -256,36 +411,17 @@ class DCOptimalPowerFlow(object):
     def _create_map_ids_to_values(ids, values):
         return {idx: value for idx, value in zip(ids, values)}
 
+    @staticmethod
+    def _access_pyomo_variable(var):
+        return np.array([pyo.value(var[idx]) for idx in var])
 
-def update_backend(env, verbose=False):
-    """
-    Update backend grid with missing data.
-    """
-    grid = env.backend._grid
-
-    if env.name == "rte_case5_example":
-        # Loads
-        grid.load["controllable"] = False
-
-        # Generators
-        grid.gen["controllable"] = True
-        grid.gen["min_p_mw"] = env.gen_pmin
-        grid.gen["max_p_mw"] = env.gen_pmax
-
-        # Additional data
-        # Not used for the time being
-        # grid.gen["type"] = env.gen_type
-        # grid.gen["gen_redispatchable"] = env.gen_redispatchable
-        # grid.gen["gen_max_ramp_up"] = env.gen_max_ramp_up
-        # grid.gen["gen_max_ramp_down"] = env.gen_max_ramp_down
-        # grid.gen["gen_min_uptime"] = env.gen_min_uptime
-        # grid.gen["gen_min_downtime"] = env.gen_min_downtime
-
-    if verbose:
-        print(env.name.upper())
-        print("bus\n" + grid.bus.to_string())
-        print("gen\n" + grid.gen.to_string())
-        print("load\n" + grid.load.to_string())
-        print("line\n" + grid.line.to_string())
+    @staticmethod
+    def _print_res_gen(gen_p, gen_costs):
+        res_gen = pd.DataFrame()
+        res_gen["gen_p_mw"] = gen_p
+        res_gen["gen_cost_per_mw"] = gen_costs
+        print(res_gen.to_string())
 
 
+class StandardDCOPF(object):
+    pass